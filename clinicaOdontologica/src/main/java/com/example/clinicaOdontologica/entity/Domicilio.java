d664c52ede18449cb9cfa47b6614f033f83e86de
Math92 <mathiassan92@gmail.com>
2024-08-31 15:01:19 -0300

    creando entidades

--- clinicaOdontologica/src/main/java/com/example/clinicaOdontologica/entity/Domicilio.java
+++ clinicaOdontologica/src/main/java/com/example/clinicaOdontologica/entity/Domicilio.java
@@ -1,6 +1,16 @@
 package com.example.clinicaOdontologica.entity;
 
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
 public class Domicilio {
     private Integer id;
     private String calle;
@@ -8,58 +18,4 @@ public class Domicilio {
     private String localidad;
     private String provincia;
 
-    public Domicilio(Integer id, String calle, Integer numero, String localidad, String provincia) {
-        this.id = id;
-        this.calle = calle;
-        this.numero = numero;
-        this.localidad = localidad;
-        this.provincia = provincia;
-    }
-
-    public Domicilio(String calle, Integer numero, String localidad, String provincia) {
-        this.calle = calle;
-        this.numero = numero;
-        this.localidad = localidad;
-        this.provincia = provincia;
-    }
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getCalle() {
-        return calle;
-    }
-
-    public void setCalle(String calle) {
-        this.calle = calle;
-    }
-
-    public Integer getNumero() {
-        return numero;
-    }
-
-    public void setNumero(Integer numero) {
-        this.numero = numero;
-    }
-
-    public String getLocalidad() {
-        return localidad;
-    }
-
-    public void setLocalidad(String localidad) {
-        this.localidad = localidad;
-    }
-
-    public String getProvincia() {
-        return provincia;
-    }
-
-    public void setProvincia(String provincia) {
-        this.provincia = provincia;
-    }
 }
--- clinicaOdontologica/src/main/java/com/example/clinicaOdontologica/entity/Turno.java
+++ clinicaOdontologica/src/main/java/com/example/clinicaOdontologica/entity/Turno.java
@@ -1,14 +1,17 @@
 package com.example.clinicaOdontologica.entity;
 
+import jakarta.persistence.Entity;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
-
 import java.time.LocalDate;
 
 @Getter
 @Setter
 @AllArgsConstructor
+@NoArgsConstructor
+@Entity
 public class Turno {
     private Integer id;
     private Paciente paciente;
