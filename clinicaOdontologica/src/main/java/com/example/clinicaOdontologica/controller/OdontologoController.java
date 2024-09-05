22975ecce4de42294c1c0ff5dd07ce72df61c2c5
helenvasquezsanchez@gmail.com <helenvasquezsanchez@gmail.com>
2024-09-05 13:37:12 -0500

    logs odontologo

--- clinicaOdontologica/src/main/java/com/example/clinicaOdontologica/controller/OdontologoController.java
+++ clinicaOdontologica/src/main/java/com/example/clinicaOdontologica/controller/OdontologoController.java
@@ -2,6 +2,8 @@ package com.example.clinicaOdontologica.controller;
 
 import com.example.clinicaOdontologica.entity.Odontologo;
 import com.example.clinicaOdontologica.service.OdontologoService;
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -16,28 +18,48 @@ public class OdontologoController {
     @Autowired
     private OdontologoService odontologoService;
 
+    private static final Logger logger = LogManager.getLogger(OdontologoController.class);
+
     // Register a new odontologist
     @PostMapping
     public ResponseEntity<Odontologo> registrarOdontologo(@RequestBody Odontologo odontologo) {
-        return ResponseEntity.ok(odontologoService.registrarOdontologo(odontologo));
+        logger.info("Registrando nuevo odontologo: " + odontologo);
+        Odontologo odontologoRegistrado = odontologoService.registrarOdontologo(odontologo);
+        logger.info("Odontologo registrado con éxito: " + odontologoRegistrado);
+        return ResponseEntity.ok(odontologoRegistrado);
     }
 
     // Find all odontologists
     @GetMapping
     public ResponseEntity<List<Odontologo>> buscarTodos() {
-        return ResponseEntity.ok(odontologoService.buscarTodos());
+        logger.info("Buscando todos los odontologos." );
+        List<Odontologo> odontologos = odontologoService.buscarTodos();
+        logger.info("Se encontraron " + odontologos.size() + " odontologos.");
+        return ResponseEntity.ok(odontologos);
     }
 
     // Find an odontologist by ID
     @GetMapping("/{id}")
     public ResponseEntity<Optional<Odontologo>> buscarPorId(@PathVariable Integer id) {
-        return ResponseEntity.ok(odontologoService.buscarPorId(id));
+        logger.info("Buscando odontologo con ID: " + id);
+        Optional<Odontologo> odontologoEncontrado = odontologoService.buscarPorId(id);
+        if (odontologoEncontrado.isPresent()) {
+            logger.info("Odontologo encontrado: " + odontologoEncontrado.get());
+        } else {
+            logger.warn("No se encontró un odontologo con ID: "+ id);
+        }
+        return ResponseEntity.ok(odontologoEncontrado);
     }
 
     // Find an odontologist by name
     @GetMapping("/buscarPorNombre/{nombre}")
     public ResponseEntity<Optional<Odontologo>> buscarPorNombre(@PathVariable String nombre) {
-        return ResponseEntity.ok(odontologoService.buscarPorNombre(nombre));
+        logger.info("Buscando odontologo con nombre: " + nombre);
+        Optional<Odontologo> odontologoEncontrado = odontologoService.buscarPorNombre(nombre);
+        if (odontologoEncontrado.isPresent()) {
+            logger.info("Odontologo encontrado: ");
+        }
+        return ResponseEntity.ok(odontologoEncontrado);
     }
 
     // Find an odontologist by matricula (license number)
